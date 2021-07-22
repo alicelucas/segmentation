d535fd086bc2fb94f319f8507d8f6fee877eed49
alucas <alicelucas93@gmail.com>
2021-07-22 15:28:10 -0400

    Correct loading of model

--- models/model.py
+++ models/model.py
@@ -14,8 +14,8 @@ def unet_model():
     # base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
 
     #Instantiate model using stored weights
-    base_model = tf.keras.applications.mobilenet_v2.MobileNetV2(input_shape=[128, 128, 3], include_top=False) #Instantiate architecture
-    base_model.load_weights("./weights/mobilenet_v2_weights_tf_dim_ordering_tf_kernels_1.0_128_no_top.h5")
+    base_model = tf.keras.applications.mobilenet_v2.MobileNetV2(input_shape=[128, 128, 3], include_top=False,
+                                                                weights="./weights/mobilenet_v2_weights_tf_dim_ordering_tf_kernels_1.0_128_no_top.h5") #Instantiate architecture
 
     # Use the activations of these layers
     layer_names = [

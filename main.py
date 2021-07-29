73f920d209398d18ae647fa5a8851f41301dae65
alucas <alicelucas93@gmail.com>
2021-07-29 14:15:43 -0400

    Moved inference code to its own module

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from os import listdir
 from os.path import isfile, join
 
-import numpy
 from skimage import io, color
 
-from models import model
 from preprocessing import Cells
+from test import test
 from utils import preprocessing
 
 if __name__ == '__main__':
@@ -34,40 +33,10 @@ if __name__ == '__main__':
         # print(color.label2rgb(y[0,:,:,0]).shape)
         io.imsave(f"./images/y.{filenames[i]}_tmp.png", color.label2rgb(y[i,:,:,0]))
 
-    #Initialize model
-    unet = model.unet_model()
+    # Make inference pass
+    probs = test.forward_pass(x, "unet", batch_size, num_classes)
 
-    #Code below prepares patch extraction process
-    pad_size = 8
-    input_size = 224
-    patch_size = input_size - 2 * pad_size # account for padding
-
-    # number of patches in row and column directions
-    n_row = x.shape[2] // patch_size
-    n_col = x.shape[1] // patch_size
-
-    #pad whole image so that we can account for border effects
-    pad_row = int(numpy.floor((n_row + 1) * patch_size - x.shape[2]) / 2)
-    pad_col = int(numpy.floor((n_col + 1) * patch_size - x.shape[1]) / 2)
-
-    im = numpy.pad(x, ((0, 0), (pad_col, pad_col), (pad_row, pad_row), (0, 0)))
-
-    probs = numpy.zeros((batch_size, (n_col + 1) * patch_size, (n_row + 1) * patch_size, num_classes)) #Probability map for the whole image
-
-    #Extract patches over image
-    for i in range(n_row + 1):
-        print(f"Prediction row {i} out of {n_row} rows.")
-        for j in range(n_col + 1):
-            patch = im[:, patch_size*i:patch_size*(i+1), patch_size*j:patch_size*(j+1), :] #extract patch
-            patch = numpy.pad(patch, ((0, 0), (8, 8), (8, 8), (0, 0))) #add padding
-            patch_prob = unet.predict(patch) #forward pass
-            probs[:, patch_size*i:patch_size*(i+1), patch_size*j:patch_size*(j+1), :] = patch_prob[:, pad_size: input_size - pad_size, pad_size: input_size - pad_size, :]
-
-
-    #Crop probability mask to original image size
-    probs = probs[:, pad_col:-pad_col, pad_row:-pad_row, :]
-
-    #Convert whole probability map to color mask
+    # Convert whole probability map to color mask for each example in image
     for i in range(probs.shape[0]):
         mask = preprocessing.prob_to_mask(probs[i])
         io.imsave(f'images/mask.{filenames[i]}.png', mask)
---
+++ test/test.py
@@ -0,0 +1,52 @@
+import numpy
+from models import model
+
+def forward_pass(x, model_name, batch_size, num_classes):
+    """
+    Given input, forward pass through model. If needed, patch up image.
+    :param input: (B, N, M, C) input
+    :param model: string for model name
+    :param batch_size
+    :param num_classes
+    :return: The probability map of the size of the input image
+    """
+
+    if model_name == "unet":
+        # Initialize model
+        unet = model.unet_model()
+
+        # Code below prepares patch extraction process for inference when testing
+        pad_size = 8
+        input_size = 224 #MobileNet encoder expects 225 for input size
+        patch_size = input_size - 2 * pad_size  # account for padding
+
+        # number of patches in row and column directions
+        n_row = x.shape[2] // patch_size
+        n_col = x.shape[1] // patch_size
+
+        # pad whole image so that we can account for border effects
+        pad_row = int(numpy.floor((n_row + 1) * patch_size - x.shape[2]) / 2)
+        pad_col = int(numpy.floor((n_col + 1) * patch_size - x.shape[1]) / 2)
+
+        im = numpy.pad(x, ((0, 0), (pad_col, pad_col), (pad_row, pad_row), (0, 0)))
+
+        probs = numpy.zeros((batch_size, (n_col + 1) * patch_size, (n_row + 1) * patch_size,
+                             num_classes))  # Probability map for the whole image
+
+        # Extract patches over image
+        for i in range(n_row + 1):
+            print(f"Prediction row {i} out of {n_row} rows.")
+            for j in range(n_col + 1):
+                patch = im[:, patch_size * i:patch_size * (i + 1), patch_size * j:patch_size * (j + 1),
+                        :]  # extract patch
+                patch = numpy.pad(patch, ((0, 0), (8, 8), (8, 8), (0, 0)))  # add padding
+                patch_prob = unet.predict(patch)  # forward pass
+                probs[:, patch_size * i:patch_size * (i + 1), patch_size * j:patch_size * (j + 1), :] = patch_prob[:,
+                                                                                                        pad_size: input_size - pad_size,
+                                                                                                        pad_size: input_size - pad_size,
+                                                                                                        :]
+
+        # Crop probability mask to original image size
+        probs = probs[:, pad_col:-pad_col, pad_row:-pad_row, :]
+
+    return probs

8e936d7ea9e8d6aba18b641f35653b5f43ba0af8
alucas <alicelucas93@gmail.com>
2021-08-30 11:58:02 -0400

    change function to accept arguments

--- utils/to_semantic.py
+++ utils/to_semantic.py
@@ -2,19 +2,28 @@ import os
 from PIL import Image
 import numpy as np
 
-def convert_DSB_to_semantic_masks():
+def convert_DSB_to_semantic_masks(dataset_path, out_path):
     """
     The DSB dataset has multiple masks for a given image, since it provides the instance segmentation problem.
     Go through the dataset and make a new one such that it corresponds to the semantic format: for each image,
     we have a single mask. The mask will be binary in this case.
+    Expects that the dataset_path folder has a folder of "images" with one image file
+    And a folder of "masks" with multiple masks for each image (see DSB as example).
+
+    e.g dataset_path = = "data/2018-DSB/stage1_train"
+    e.g. out_path = "data/2018-DSB/semantic/train"
     """
 
-    dataset_path = "data/2018-DSB/stage1_train"
-    out_path = "data/2018-DSB/semantic/train"
+    #Write output directories
     out_images_path = os.path.join(out_path, "images")
     out_masks_path = os.path.join(out_path, "masks")
 
-    num_images = len(os.listdir(dataset_path))
+    if not os.path.exists(out_images_path):
+        os.makedirs(out_images_path)
+    if not os.path.exists(out_masks_path):
+        os.makedirs(out_masks_path)
+
+    num_images = len(os.listdir(dataset_path)) #Used for logging
 
     for i, subdir in enumerate(os.listdir(dataset_path)):
 

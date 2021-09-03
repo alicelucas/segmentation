b4012fe924d688171804a711f0c1ea975fa510b7
alucas <alicelucas93@gmail.com>
2021-09-03 10:54:47 -0400

    Fix bugs in import script

--- from_chtc.sh
+++ from_chtc.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-EXPERIMENT=$(grep -A3 'experiment_name:' config/config.yaml | head -n 1 | sed 's/.*: //')
-PROJECT=$(grep -A3 'project' config/config.yaml | head -n 1 | sed 's/.*: //')
+scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/segmentation/config/config.yaml ~/Documents/com/github/segmentation/tmp/
+
+
+EXPERIMENT=$(grep -A3 'experiment_name:' tmp/config.yaml | head -n 1 | sed 's/.*: //')
+PROJECT=$(grep -A3 'project' tmp/config.yaml | head -n 1 | sed 's/.*: //')
 cd experiments
-mkdir ${PROJECT}
+mkdir -p ${PROJECT}
 cd ${PROJECT}
-mkdir ${EXPERIMENT}
+mkdir -p ${EXPERIMENT}
 cd ../../
 scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/loss.png ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}
 scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/unet.h5 ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}
-scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/commit.txt ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}
+scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/commit.txt ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT} 
+mv tmp/config.yaml ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}/

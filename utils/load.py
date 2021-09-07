10f9c3693d1037c80da8fb2bd7cbf0cbcd2d336d
alucas <alicelucas93@gmail.com>
2021-09-07 14:40:29 -0400

    removed hardcoding of config file

--- utils/load.py
+++ utils/load.py
@@ -1,11 +1,8 @@
-from os.path import join
 import yaml
-
-CONFIG_PATH = "./config"
 
 def load_config(config_name):
 
-    with open(join(CONFIG_PATH, config_name)) as file:
+    with open(config_name) as file:
         config = yaml.safe_load(file)
 
     return config

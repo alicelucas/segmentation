f86ab9570850b3598a0c24d60d5d490a0f3de853
alucas <alicelucas93@gmail.com>
2021-09-08 11:23:33 -0400

    correction to import script

--- from_chtc.sh
+++ from_chtc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/segmentation/config/config.yaml ~/Documents/com/github/segmentation/tmp/
+scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/$1/config.yaml ~/Documents/com/github/segmentation/tmp/
 
 
 EXPERIMENT=$(grep -A3 'experiment_name:' tmp/config.yaml | head -n 1 | sed 's/.*: //')
@@ -10,7 +10,7 @@ mkdir -p ${PROJECT}
 cd ${PROJECT}
 mkdir -p ${EXPERIMENT}
 cd ../../
-scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/loss.png ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}
-scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/unet.h5 ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}
-scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/commit.txt ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT} 
+scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/$1/loss.png ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}
+scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/$1/unet.h5 ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}
+scp amlucas3@submit1.chtc.wisc.edu:/home/amlucas3/$1/commit.txt ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT} 
 mv tmp/config.yaml ~/Documents/com/github/segmentation/experiments/${PROJECT}/${EXPERIMENT}/

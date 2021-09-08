ead7486d08df5004fdc1d0c8945f9dd8894a16af
alucas <alicelucas93@gmail.com>
2021-09-08 10:01:31 -0400

    tmp debug code

--- utils/load.py
+++ utils/load.py
@@ -1,12 +1,13 @@
 import yaml
 import os
+import logging
 
 def load_config(config_name):
 
     #DEBUG WORK
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
     for f in files:
-        print(f)
+        logging.error(f)
 
     with open(config_name) as file:
         config = yaml.safe_load(file)
